b59a41580f96bda6cdc8ddd1e172a547c68a48f9
SeiCp <seycuartero@gmail.com>
2025-04-12 10:42:10 +0200

    add changes js

---
+++ Ejercicio-1/js/main.js
@@ -0,0 +1,17 @@
+/** Paso 1
+*1. Crear en el html el p
+*2. Añadir clase en js: js_parrafo
+*3. Traer el elemento desde html por su clase
+*4. Modificar el p con el texto: Hola Mundo
+*/
+
+/** Paso 2
+ * 1. <p>
+ * 2. <p class="ks_parrafo> </p>"
+ * 3. Traer el elemento:
+ *   3.1
+ *   3.2
+*/
+
+const elementoParrafo = document.querySelector(".js_parrafo");
+elementoParrafo.innerHTML = "Hola Mundo";
