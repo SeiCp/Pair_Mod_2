1e83c6a2fb05f9ba1b5011b9601429d84a9fb84e
SeiCp <seycuartero@gmail.com>
2025-04-16 21:30:02 +0200

    function last part

--- Funciones-1/js/main.js
+++ Funciones-1/js/main.js
@@ -9,9 +9,7 @@ function multiplicacion (){
 }
 /**
  * 
- * @param {any} 
- * @param {any} 
- * @returns 
+ *
  */
 
 
@@ -19,11 +17,23 @@ function multiplicacion(numero1, numero2){
     return numero1 * numero2;
 }
 
-console.log(multiplicacion(2, 4))
+
+let primerNumero = 2;
+let segundoNumero = 4;
+
+console.log(multiplicacion(primerNumero, segundoNumero))
 
 
 function media(numero1, numero2, numero3, numero4){
     return((numero1 + numero2 + numero3 + numero4)/4)
 }
 
-console.log(media(2, 4, 6, 8))
+console.log(media(2, 4, 6, 8)) 
+
+function esPar(numero){
+   return numero % 2 === 0
+}
+
+const resultado = esPar(6)
+
+console.log(resultado)
