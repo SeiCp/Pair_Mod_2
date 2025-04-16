e53258c030dc63d56fde576f3371823de014e660
SeiCp <seycuartero@gmail.com>
2025-04-16 20:42:07 +0200

    excersices function 1

---
+++ Funciones-1/js/main.js
@@ -0,0 +1,29 @@
+
+/*
+function multiplicacion (){
+    let numero1 = 2;
+    let numero2 = 4;
+    
+    let multiplicacion = numero1 * numero2;
+    console.log (multiplicacion);
+}
+/**
+ * 
+ * @param {any} 
+ * @param {any} 
+ * @returns 
+ */
+
+
+function multiplicacion(numero1, numero2){
+    return numero1 * numero2;
+}
+
+console.log(multiplicacion(2, 4))
+
+
+function media(numero1, numero2, numero3, numero4){
+    return((numero1 + numero2 + numero3 + numero4)/4)
+}
+
+console.log(media(2, 4, 6, 8))
