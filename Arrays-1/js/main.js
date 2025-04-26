382d81948958b4e46ab7d56adb88660d2fe109d1
SeiCp <seycuartero@gmail.com>
2025-04-26 10:52:36 +0200

    arrays

---
+++ Arrays-1/js/main.js
@@ -0,0 +1,32 @@
+
+const listaPeliculas = [
+  "Amelie",
+  "Bichos",
+  "La vida es bella"
+]
+
+listaPeliculas[3]="Titanic"
+
+console.log(listaPeliculas)
+
+listaPeliculas[1] = "Cars"
+
+console.log(listaPeliculas)
+
+function workWithMovies () {
+  const listaPeliculas = [
+    "Amelie",
+    "Bichos",
+    "La vida es bella"
+  ]
+  
+  listaPeliculas[3]="Titanic"
+  
+  console.log(listaPeliculas)
+  
+  listaPeliculas[1] = "Cars"
+  
+  console.log(listaPeliculas)
+  
+}
+workWithMovies ()
